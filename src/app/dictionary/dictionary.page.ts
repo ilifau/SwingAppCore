b66ae8d295dd0777d1103795de7db14ca32e7891
Fred Neumann <fred.neumann@fau.de>
2019-12-10 00:22:32 +0100

    renamed tabs

---
+++ src/app/dictionary/dictionary.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dictionary',
+  templateUrl: 'dictionary.page.html',
+  styleUrls: ['dictionary.page.scss']
+})
+export class DictionaryPage {
+
+  constructor() {}
+
+}
--- src/app/training/training.page.spec.ts
+++ src/app/training/training.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { Tab3Page } from './tab3.page';
+import { TrainingPage } from './training.page';
 
-describe('Tab3Page', () => {
-  let component: Tab3Page;
-  let fixture: ComponentFixture<Tab3Page>;
+describe('TrainingPage', () => {
+  let component: TrainingPage;
+  let fixture: ComponentFixture<TrainingPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab3Page],
+      declarations: [TrainingPage],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab3Page);
+    fixture = TestBed.createComponent(TrainingPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
