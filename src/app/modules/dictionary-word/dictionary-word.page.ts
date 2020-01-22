f15aab53546c28009d8a766eaffdd4c955e0d553
Fred Neumann <fred.neumann@fau.de>
2020-01-22 02:18:52 +0100

    solution for video on ios

--- src/app/modules/dictionary-word/dictionary-word.page.ts
+++ src/app/modules/dictionary-word/dictionary-word.page.ts
@@ -33,9 +33,39 @@ export class DictionaryWordPage implements OnInit {
 
     this.dictService.getWord(wordId).subscribe((data: any) => {
       this.word = data;
+      this.getVideo();
     });
 
     this.ios = this.config.get('mode') === 'ios';
+  }
+
+  /**
+   * Each time the page is shown
+   */
+  ionViewWillEnter() {
+
+  }
+
+
+  getVideo() {
+    var xhr = new XMLHttpRequest();
+    var word = this.word;
+    xhr.open('GET', 'content/'+ word.videoName, true);
+    xhr.responseType = 'blob'; //important
+    xhr.onload = function(e) {
+      if (this.status == 200) {
+        console.log("loaded");
+        var blob = this.response;
+        var video:any = document.getElementById('dictionaryVideo'+ word.id);
+        video.oncanplaythrough = function() {
+          console.log("Can play through video without stopping");
+          URL.revokeObjectURL(this.src);
+        };
+        video.src = URL.createObjectURL(blob);
+        video.load();
+      }
+    };
+    xhr.send();
   }
 
 }
