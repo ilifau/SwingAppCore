b50f7ac1d021f316aa42b8a665970888c4f7384c
Fred Neumann <fred.neumann@fau.de>
2020-01-22 01:50:34 +0100

    attempt to support video on ios

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,3 +64,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+// getUserMedia patch
+import 'zone.js/dist/zone-patch-user-media';
